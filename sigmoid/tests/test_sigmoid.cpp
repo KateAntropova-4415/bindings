99a7e983c0b9fc807e661b37a1d6f7eace294dbb
Ekaterina Antropova <56113701+KateAntropova-4415@users.noreply.github.com>
2024-11-04 21:55:47 +0300

    Update test_sigmoid.cpp

--- sigmoid/tests/test_sigmoid.cpp
+++ sigmoid/tests/test_sigmoid.cpp
@@ -1,14 +1,15 @@
 #include "Sigmoid.h"
 #include <gtest/gtest.h>
+#include <cmath>
 
 TEST(Sigmoid, SigmoidForVector) {
 	std::vector<double> a = { 1.0, 2.0, 3.0, 4.0, 5.0 };
 	auto result = Sigmoid::sigmoid_vector(a);
-	EXPECT_DOUBLE_EQ(result[0], 0.731059);
-	EXPECT_DOUBLE_EQ(result[1], 0.880797);
-	EXPECT_DOUBLE_EQ(result[2], 0.952574);
-	EXPECT_DOUBLE_EQ(result[3], 0.982014);
-	EXPECT_DOUBLE_EQ(result[4], 0.993307);
+	EXPECT_DOUBLE_EQ(round(result[0]*1000000)/1000000, 0.731059);
+	EXPECT_DOUBLE_EQ(round(result[1]*1000000)/1000000, 0.880797);
+	EXPECT_DOUBLE_EQ(round(result[2]*1000000)/1000000, 0.952574);
+	EXPECT_DOUBLE_EQ(round(result[3]*1000000)/1000000, 0.982014);
+	EXPECT_DOUBLE_EQ(round(result[4]*1000000)/1000000, 0.993307);
 }
 
 int main(int argc, char** argv) {
